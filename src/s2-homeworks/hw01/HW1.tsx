74be5cb176a5e7e1ea014eb75e10a5ce13a90b7b
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2025-01-20 14:05:48 +0300

    refactor

---
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Message from './message/Message'
+import MessageSender from './message-sender/MessageSender'
+import s2 from '../../s1-main/App.module.css'
+import FriendMessage from './friend-message/FriendMessage'
+import avatar from './avatar.png'
+
+/*
+* 1 - описать тип MessageType
+* 2 - описать тип MessagePropsType в файле Message.tsx
+* 3 - в файле Message.tsx отобразить приходящие данные
+* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
+* 5 - сделать стили в соответствии с дизайном
+* */
+
+// нужно создать правильный тип вместо any
+export  type  MessageTypeProps= {
+    avatar: string,
+    name: string,
+}
+
+export  type  MessageTypeProps2= {
+    text: string,
+    time: string,
+}
+
+export type MessageType = {
+    id: number,
+    user: MessageTypeProps,
+    message:  MessageTypeProps2
+}
+
+// структуру объекта не менять
+export const message0: MessageType = {
+    id: 0,
+    user: {
+        avatar: avatar, // можно менять
+        name: 'Ksenia Malysheva',  // можно менять
+    },
+    message: {
+        text: 'Привет! Ты победитель!', // можно менять
+        time: '22:00', // можно менять
+    },
+}
+export const friendMessage0: MessageType = {
+    id: 100,
+    user: {
+        avatar: avatar, // можно менять
+        name: 'Malyshev Dmitrii', // можно менять
+    },
+    message: {
+        text: 'Ты первый!', // можно менять
+        time: '23:00', // можно менять
+    },
+}
+
+const HW1 = () => {
+    return (
+        <div id={'hw1'}>
+            <div className={s2.hwTitle}>Homework #1</div>
+            <div className={s2.hw}>
+                {/*проверка отображения (не менять)*/}
+                <div>
+                    <Message message={message0} />
+                    <FriendMessage message={friendMessage0} />
+                </div>
+
+                {/*для автоматической проверки дз (не менять)*/}
+                <MessageSender M={Message} />
+            </div>
+        </div>
+    )
+}
+
+export default HW1
