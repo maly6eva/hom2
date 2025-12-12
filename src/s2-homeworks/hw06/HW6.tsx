b6f8c70c1d12c9bc77544733a73eb6e2ff5bc4a7
Malysheva Ksenia <maly6eva.ksenia@gmail.com>
2025-12-12 15:04:54 +0300

    dom6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,6 +18,8 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
+        const valueFromLS = restoreState<string>('hw6-editable-span-value', '')
+        setValue(valueFromLS)
         // делают студенты
 
     }
