71e0d060e87cd9da70d8e0781ea4adfd4be92b24
Malysheva Ksenia <maly6eva.ksenia@gmail.com>
2026-02-21 06:04:32 +0300

    обновление

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -13,42 +13,54 @@ import {Loader} from './Loader'
 * 4 - сделать стили в соответствии с дизайном
 * */
 
-
-const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
+export const HW10 = () => {
 
     const dispatch = useDispatch()
-    const isLoading = useSelector<AppStoreType, boolean>(
-        state => state.loading.isLoading
+
+    const isLoading = useSelector(
+        (state: AppStoreType) => state.loading.isLoading
     )
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
+    const setLoading = () => {
+
         dispatch(loadingAC(true))
+
         setTimeout(() => {
+
             dispatch(loadingAC(false))
+
         }, 1500)
+
     }
 
     return (
         <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
+
+            <div className={s2.hwTitle}>
+                Homework #10
+            </div>
 
             <div className={s2.hw}>
+
                 {isLoading ? (
+
                     <div id={'hw10-loading'}>
                         <Loader/>
                     </div>
+
                 ) : (
+
                     <SuperButton
                         id={'hw10-button-start-loading'}
                         onClick={setLoading}
                     >
                         Set loading...
                     </SuperButton>
+
                 )}
+
             </div>
+
         </div>
     )
 }
-
-export default HW10
