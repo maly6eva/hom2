2eaff1d6f6ba795f0e4e2c2eabf8753245876cc7
Malysheva Ksenia <maly6eva.ksenia@gmail.com>
2026-02-26 15:18:38 +0300

    обновление

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,43 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: 200,
+
+                color: '#00C853',
+
+                '& .MuiSlider-track': {
+                    backgroundColor: '#00C853',
+                    height: 4,
+                },
+
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#9E9E9E',
+                    height: 4,
+                    opacity: 1,
+                },
+
+                '& .MuiSlider-thumb': {
+
+                    width: 20,
+                    height: 20,
+
+                    backgroundColor: '#fff',
+
+                    border: '3px solid #00C853',
+
+                    '&:hover': {
+                        boxShadow: '0 0 0 8px rgba(0,200,83,0.16)',
+                    },
+
+                    '&.Mui-focusVisible': {
+                        boxShadow: '0 0 0 8px rgba(0,200,83,0.16)',
+                    },
+
+                    '&:before': {
+                        display: 'none',
+                    },
+
+                },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
